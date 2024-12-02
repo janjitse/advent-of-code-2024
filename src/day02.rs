76a69b69c52495b9ac08db3a5b5c2eb883c713c5
Jan Jitse <janjitse@gmail.com>
2024-12-02 10:07:23 +0100

    A faster solution (at least for large rows) using a recursion on the number of mistakes still allowed.

--- src/day02.rs
+++ src/day02.rs
@@ -17,13 +17,13 @@ pub fn part_a(contents: &str) -> i32 {
     return total_safe;
 }
 
-#[aoc(day2, part2)]
+#[aoc(day2, part2, brute_force)]
 pub fn part_b(contents: &str) -> i32 {
     let mut todo = parse(contents);
     let mut total_safe = 0;
 
     for t in todo.iter_mut() {
-        let mut safe = false;        
+        let mut safe = false;
         if check_all_ascending(t.to_vec()) || check_all_descending(t.to_vec()) {
             safe = true;
         } else {
@@ -42,6 +42,42 @@ pub fn part_b(contents: &str) -> i32 {
         }
     }
     return total_safe;
+}
+
+#[aoc(day2, part2, recursing)]
+pub fn part_b_recur(contents: &str) -> i32 {
+    let mut todo = parse(contents);
+    let mut total_safe = 0;
+    for t in todo.iter_mut() {
+        let mut safe = false;
+        safe |= check_all_descending_rec(t, 1);
+        let mut t_rev = t.iter().map(|x| *x).rev().collect::<Vec<i32>>();
+        safe |= check_all_descending_rec(&mut t_rev, 1);
+        if safe {
+            total_safe += 1;
+        }
+    }
+    return total_safe;
+}
+
+fn check_all_descending_rec(t: &mut Vec<i32>, mistakes_allowed: i32) -> bool {
+    let mut orig_t = t.clone();
+    for (idx, w) in t.windows(2).enumerate() {
+        if !((1 <= w[1] - w[0]) && (w[1] - w[0] <= 3)) {
+            if mistakes_allowed > 0 {
+                let mut safe = false;
+                for removal in [0, 1] {
+                    let removed = orig_t.remove(idx + removal);
+                    safe |= check_all_descending_rec(&mut orig_t, mistakes_allowed - 1);
+                    orig_t.insert(idx + removal, removed)
+                }
+                return safe;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 fn check_all_descending(t: Vec<i32>) -> bool {
