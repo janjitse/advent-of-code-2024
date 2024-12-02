a3a7078fa60dfb5d2e0e1d252a74c8b4cc3ce476
Jan Jitse <janjitse@gmail.com>
2024-12-02 11:59:51 +0100

    Rayon all the things (only really useful for very large inputs, much larger than the real puzzle input)

--- src/day02.rs
+++ src/day02.rs
@@ -1,19 +1,12 @@
+use rayon::prelude::*;
+
 #[aoc(day2, part1)]
 pub fn part_a(contents: &str) -> i32 {
     let todo = parse(contents);
-    let mut total_safe = 0;
-    for t in todo {
-        let safe: bool;
-        if t[0] > *t.last().unwrap() {
-            safe = check_all_descending(t);
-        } else {
-            safe = check_all_ascending(t);
-        }
-        if safe {
-            total_safe += 1;
-        }
-    }
-
+    let total_safe = todo
+        .par_iter()
+        .filter(|&t| check_all_descending(t) || check_all_ascending(t))
+        .count() as i32;
     return total_safe;
 }
 
@@ -24,12 +17,12 @@ pub fn part_b(contents: &str) -> i32 {
 
     for t in todo.iter_mut() {
         let mut safe = false;
-        if check_all_ascending(t.to_vec()) || check_all_descending(t.to_vec()) {
+        if check_all_ascending(t) || check_all_descending(t) {
             safe = true;
         } else {
             for mask in 0..t.len() {
                 let removed = t.remove(mask);
-                if check_all_ascending(t.to_vec()) || check_all_descending(t.to_vec()) {
+                if check_all_ascending(t) || check_all_descending(t) {
                     safe = true;
                     break;
                 }
@@ -46,29 +39,29 @@ pub fn part_b(contents: &str) -> i32 {
 
 #[aoc(day2, part2, recursing)]
 pub fn part_b_recur(contents: &str) -> i32 {
-    let mut todo = parse(contents);
-    let mut total_safe = 0;
-    for t in todo.iter_mut() {
-        let mut safe = false;
-        safe |= check_all_descending_rec(t, 1);
-        let mut t_rev = t.iter().map(|x| *x).rev().collect::<Vec<i32>>();
-        safe |= check_all_descending_rec(&mut t_rev, 1);
-        if safe {
-            total_safe += 1;
-        }
-    }
+    let todo = parse(contents);
+    let total_safe = todo
+        .par_iter()
+        .filter(|&t| {
+            check_all_descending_rec(t, 1)
+                || check_all_descending_rec(
+                    &t.iter().map(|x| *x).rev().collect::<Vec<i32>>(),
+                    1,
+                )
+        })
+        .count() as i32;
     return total_safe;
 }
 
-fn check_all_descending_rec(t: &mut Vec<i32>, mistakes_allowed: i32) -> bool {
-    let mut orig_t = t.clone();
+fn check_all_descending_rec(t: &Vec<i32>, mistakes_allowed: i32) -> bool {
     for (idx, w) in t.windows(2).enumerate() {
         if !((1 <= w[1] - w[0]) && (w[1] - w[0] <= 3)) {
             if mistakes_allowed > 0 {
+                let mut orig_t = (*t).clone();
                 let mut safe = false;
                 for removal in [0, 1] {
                     let removed = orig_t.remove(idx + removal);
-                    safe |= check_all_descending_rec(&mut orig_t, mistakes_allowed - 1);
+                    safe |= check_all_descending_rec(&orig_t, mistakes_allowed - 1);
                     orig_t.insert(idx + removal, removed)
                 }
                 return safe;
@@ -80,37 +73,33 @@ fn check_all_descending_rec(t: &mut Vec<i32>, mistakes_allowed: i32) -> bool {
     return true;
 }
 
-fn check_all_descending(t: Vec<i32>) -> bool {
-    let mut safe = true;
+fn check_all_descending(t: &Vec<i32>) -> bool {
     for w in t.windows(2) {
         if w[0] <= w[1] || w[0] - 4 >= w[1] {
-            safe = false;
-            break;
+            return false
         }
     }
-    return safe;
+    return true;
 }
 
-fn check_all_ascending(t: Vec<i32>) -> bool {
-    let mut safe = true;
+fn check_all_ascending(t: &Vec<i32>) -> bool {
     for w in t.windows(2) {
         if w[0] >= w[1] || w[1] - 4 >= w[0] {
-            safe = false;
-            break;
+            return false
         }
     }
-    return safe;
+    return true;
 }
 
 pub fn parse(contents: &str) -> Vec<Vec<i32>> {
-    let mut output = vec![];
-    for line in contents.lines() {
-        let vec: Vec<i32> = line
-            .split_ascii_whitespace()
-            .map(|x| x.parse().unwrap())
-            .collect::<Vec<i32>>();
-        output.push(vec);
-    }
+    let output: Vec<Vec<i32>> = contents
+        .par_lines()
+        .map(|x| {
+            x.split_ascii_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect()
+        })
+        .collect();
     return output;
 }
 
