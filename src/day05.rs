7bca0d0c140ccbd052f35a0e94d6ba5623c4a493
Jan Jitse <janjitse@gmail.com>
2024-12-05 22:14:50 +0100

    Make the sorting a bit more robust, and crash on ambiguous sets of rules (which do not occur in the real test case)

--- src/day05.rs
+++ src/day05.rs
@@ -1,23 +1,23 @@
 fn parse(input: &str) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
-    let mut output1: Vec<Vec<i32>> = vec![];
-    let mut output2: Vec<Vec<i32>> = vec![];
-    let input_split: Vec<&str> = input.split("\n\n").collect();
-
-    for l in input_split[0].lines() {
-        let mut temp: Vec<i32> = vec![];
-        for t in l.split("|") {
-            temp.push(t.parse().unwrap());
-        }
-        output1.push(temp);
-    }
-    for l in input_split[1].lines() {
-        let temp = l
-            .split(",")
-            .into_iter()
-            .map(|x| x.parse().unwrap())
-            .collect::<Vec<i32>>();
-        output2.push(temp);
-    }
+    let mut lines = input.lines();
+    let output1 = lines
+        .by_ref()
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            line.split('|')
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+    let output2 = lines
+        .by_ref()
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            line.split(',')
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
     return (output1, output2);
 }
 
@@ -27,17 +27,28 @@ use std::collections::{HashMap, HashSet};
 fn part1(input: &str) -> i32 {
     let (x1, x2) = parse(input);
     let mut ordering_after = HashMap::new();
+    let mut ordering_before = HashMap::new();
     for x in x1.into_iter() {
         ordering_after
             .entry(x[1])
             .or_insert(HashSet::new())
             .insert(x[0]);
+        ordering_before
+            .entry(x[0])
+            .or_insert(HashSet::new())
+            .insert(x[1]);
     }
     let mut output = 0;
     for y in x2 {
         let mut is_possible = true;
         for left_right in y.windows(2) {
-            if cmp_cust(&left_right[0], &left_right[1], &ordering_after) == Ordering::Greater {
+            if cmp_cust(
+                &left_right[0],
+                &left_right[1],
+                &ordering_after,
+                &ordering_before,
+            ) == Ordering::Greater
+            {
                 is_possible = false;
                 break;
             }
@@ -49,37 +60,61 @@ fn part1(input: &str) -> i32 {
     }
     return output;
 }
+
 use std::cmp::Ordering;
 
-fn cmp_cust(a: &i32, b: &i32, ordering_after: &HashMap<i32, HashSet<i32>>) -> Ordering {
+fn cmp_cust(
+    a: &i32,
+    b: &i32,
+    ordering_after: &HashMap<i32, HashSet<i32>>,
+    ordering_before: &HashMap<i32, HashSet<i32>>,
+) -> Ordering {
     match ordering_after.get(a) {
-        None => {
-            return Ordering::Less;
-        }
+        None => {}
         Some(t) => {
             if t.contains(b) {
                 return Ordering::Greater;
             }
-            return Ordering::Less;
         }
     }
+    match ordering_before.get(a) {
+        None => {}
+        Some(t) => {
+            if t.contains(b) {
+                return Ordering::Less;
+            }
+        }
+    }
+    println!("{:?},{:?}", *a, *b);
+    unreachable!()
 }
 
 #[aoc(day5, part2)]
 fn part2(input: &str) -> i32 {
     let (x1, mut x2) = parse(input);
     let mut ordering_after = HashMap::new();
+    let mut ordering_before = HashMap::new();
     for x in x1.into_iter() {
         ordering_after
             .entry(x[1])
             .or_insert(HashSet::new())
             .insert(x[0]);
+        ordering_before
+            .entry(x[0])
+            .or_insert(HashSet::new())
+            .insert(x[1]);
     }
     let mut output = 0;
     for y in x2.iter_mut() {
         let mut is_possible = true;
         for left_right in y.windows(2) {
-            if cmp_cust(&left_right[0], &left_right[1], &ordering_after) == Ordering::Greater {
+            if cmp_cust(
+                &left_right[0],
+                &left_right[1],
+                &ordering_after,
+                &ordering_before,
+            ) == Ordering::Greater
+            {
                 is_possible = false;
                 break;
             }
@@ -87,7 +122,9 @@ fn part2(input: &str) -> i32 {
         if !is_possible {
             let mid_idx = y.len() / 2;
             output += *y
-                .select_nth_unstable_by(mid_idx, |a, b| cmp_cust(a, b, &ordering_after))
+                .select_nth_unstable_by(mid_idx, |a, b| {
+                    cmp_cust(a, b, &ordering_after, &ordering_before)
+                })
                 .1;
         }
     }
