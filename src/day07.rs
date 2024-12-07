e9d6596a1d4354eb6913b7e11f687b2e43f5e21b
Jan Jitse <janjitse@gmail.com>
2024-12-07 11:59:13 +0100

    Some micro optimizations

--- src/day07.rs
+++ src/day07.rs
@@ -21,12 +21,10 @@ fn part1(input: &str) -> u64 {
     let s = parse(input);
     let mut output = 0;
     for (target, todo_vec) in s {
-        let comb = (2 as u64).pow(todo_vec.len() as u32 - 1);
-        // println!("{:?}", comb);
+        let comb = 2u64.pow(todo_vec.len() as u32 - 1);
         for trial in 0..comb {
             let trial_digits: Vec<u64> =
                 (0..todo_vec.len() - 1).map(|n| (trial >> n) & 1).collect();
-            // println!("{:?}", trial_digits);
             let mut outcome = todo_vec[0];
             for idx in 0..trial_digits.len() {
                 match trial_digits[idx] {
@@ -58,21 +56,21 @@ fn part1_rec(input: &str) -> u64 {
     return output;
 }
 
-fn recurse_part_a(target: u64, todo_vec: &Vec<u64>) -> bool {
+fn recurse_part_a(target: u64, todo_vec: &[u64]) -> bool {
     if todo_vec.len() == 1 {
         return todo_vec[0] == target;
-    } else if todo_vec[todo_vec.len() - 1] > target {
+    }
+    let (next_trial, todo_vec_new) = todo_vec.split_last().unwrap();
+    if *next_trial > target {
         return false;
     }
-    let mut todo_new = todo_vec.clone();
-    let next_trial = todo_new.pop().unwrap();
     if target % next_trial == 0 {
-        let multi_pos = recurse_part_a(target / next_trial, &todo_new);
+        let multi_pos = recurse_part_a(target / next_trial, todo_vec_new);
         if multi_pos {
             return true;
         }
     }
-    return recurse_part_a(target - next_trial, &todo_new);
+    return recurse_part_a(target - next_trial, todo_vec_new);
 }
 
 #[aoc(day7, part2, recurse)]
@@ -86,28 +84,28 @@ fn part2_rec(input: &str) -> u64 {
     return output;
 }
 
-fn recurse_part_b(target: u64, todo_vec: &Vec<u64>) -> bool {
+fn recurse_part_b(target: u64, todo_vec: &[u64]) -> bool {
     if todo_vec.len() == 1 {
         return todo_vec[0] == target;
-    } else if todo_vec[todo_vec.len() - 1] > target {
+    }
+    let (next_trial, todo_vec_new) = todo_vec.split_last().unwrap();
+    if *next_trial > target {
         return false;
     }
-    let mut todo_new = todo_vec.clone();
-    let next_trial = todo_new.pop().unwrap();
     if target % next_trial == 0 {
-        let can_do = recurse_part_b(target / next_trial, &todo_new);
+        let can_do = recurse_part_b(target / next_trial, todo_vec_new);
         if can_do {
             return true;
         }
     }
-    let trial_length = next_trial.checked_ilog10().unwrap_or(0) + 1;
-    if target % (10 as u64).pow(trial_length) == next_trial {
-        let can_do = recurse_part_b(target/(10 as u64).pow(trial_length), &todo_new);
+    let trial_length = 10u64.pow(next_trial.checked_ilog10().unwrap_or(0) + 1);
+    if target % trial_length == *next_trial {
+        let can_do = recurse_part_b(target / trial_length, todo_vec_new);
         if can_do {
             return true;
-        } 
+        }
     }
-    return recurse_part_b(target - next_trial, &todo_new);
+    return recurse_part_b(target - next_trial, todo_vec_new);
 }
 
 fn generate_ternary(mut nr: u64, length: usize) -> Vec<u64> {
@@ -134,7 +132,7 @@ fn part2(input: &str) -> u64 {
                     1 => outcome *= todo_vec[idx + 1],
                     2 => {
                         outcome = outcome
-                            * (10 as u64).pow(todo_vec[idx + 1].checked_ilog10().unwrap_or(0) + 1)
+                            * 10u64.pow(todo_vec[idx + 1].checked_ilog10().unwrap_or(0) + 1)
                             + todo_vec[idx + 1];
                     }
                     _ => {}
