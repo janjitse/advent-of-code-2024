c09d08a8476385d07a9b1924c1c00abd20d722c3
Jan Jitse <janjitse@gmail.com>
2024-12-09 11:55:27 +0100

    Clippy + micro-optimizations

--- src/day09.rs
+++ src/day09.rs
@@ -3,7 +3,10 @@ use std::time::SystemTime;
 fn parse(input: &str) -> Vec<u32> {
     let time_start = SystemTime::now();
     let output1 = input
-        .trim().chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>();
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<u32>>();
     println!("Parsing: {:?}", time_start.elapsed().unwrap());
     output1
 }
@@ -14,17 +17,15 @@ fn part1(input: &str) -> u64 {
     // let total: u32 = x.iter().sum();
     let mut checksum_vec = vec![];
     for (idx, file_len) in x.iter().enumerate() {
-        if idx %2 == 0 {
+        if idx % 2 == 0 {
             for _ in 0..*file_len {
-                checksum_vec.push((idx/2) as isize);
+                checksum_vec.push((idx / 2) as isize);
             }
-        }
-        else {
+        } else {
             for _ in 0..*file_len {
                 checksum_vec.push(-1);
             }
         }
-
     }
     println!("{:?}", checksum_vec.len());
     let mut compacted_vec = vec![];
@@ -37,7 +38,7 @@ fn part1(input: &str) -> u64 {
                 backpointer -= 1;
             }
             if backpointer <= idx {
-                break;  
+                break;
             }
             compacted_vec.push(checksum_vec[backpointer]);
             backpointer -= 1;
@@ -61,49 +62,77 @@ struct File {
     id: isize,
 }
 
-
 #[aoc(day9, part2)]
 fn part2(input: &str) -> u64 {
     let x = parse(input);
-    // let total: u32 = x.iter().sum();
-    let mut checksum_vec = vec![];
     let mut files: Vec<File> = vec![];
     let mut freespace: Vec<File> = vec![];
     for (idx, file_len) in x.iter().enumerate() {
-        if idx %2 == 0 {
-            let start = checksum_vec.len();
-            for _ in 0..*file_len {
-                checksum_vec.push((idx/2) as isize);
+        if idx % 2 == 0 {
+            let start = files
+                .last()
+                .unwrap_or(&File {
+                    start: 0,
+                    end: 0,
+                    id: 0,
+                })
+                .end
+                .max(
+                    freespace
+                        .last()
+                        .unwrap_or(&File {
+                            start: 0,
+                            end: 0,
+                            id: 0,
+                        })
+                        .end,
+                );
+            let end = start + *file_len as usize;
+            files.push(File {
+                start,
+                end,
+                id: (idx / 2) as isize,
+            });
+        } else {
+            if *file_len == 0 {
+                continue;
             }
-            let end = checksum_vec.len();
-            files.push(File { start: start, end: end, id: (idx/2) as isize });
-
+            let start = files
+                .last()
+                .unwrap_or(&File {
+                    start: 0,
+                    end: 0,
+                    id: 0,
+                })
+                .end
+                .max(
+                    freespace
+                        .last()
+                        .unwrap_or(&File {
+                            start: 0,
+                            end: 0,
+                            id: 0,
+                        })
+                        .end,
+                );
+            let end = start + *file_len as usize;
+            freespace.push(File { start, end, id: -1 });
         }
-        else {
-            let start = checksum_vec.len();
-            for _ in 0..*file_len {
-                checksum_vec.push(-1);
-            }
-            let end = checksum_vec.len();
-            freespace.push(File { start: start, end: end, id: -1 });
-        }
-
     }
     let mut compacted_files: Vec<File> = vec![];
-    while !files.is_empty() {
-        let next_file = files.pop().unwrap();
-        match find_next_free(next_file.end - next_file.start, &freespace) {
+    while let Some(next_file) = files.pop() {
+        match find_next_free(next_file.end - next_file.start, &freespace, next_file.start) {
             None => {
                 compacted_files.push(next_file);
             }
             Some(idx) => {
                 let new_start = freespace[idx].start;
                 let new_end = freespace[idx].start + next_file.end - next_file.start;
-                if new_start > next_file.start {
-                    compacted_files.push(next_file);
-                    continue;
-                }
-                let new_file = File{start: new_start, end: new_end, id: next_file.id};
+                let new_file = File {
+                    start: new_start,
+                    end: new_end,
+                    id: next_file.id,
+                };
                 compacted_files.push(new_file);
                 if new_end < freespace[idx].end {
                     freespace[idx].start = new_end;
@@ -111,43 +140,40 @@ fn part2(input: &str) -> u64 {
                     freespace.remove(idx);
                 }
                 let free_idx = freespace.binary_search(&next_file).unwrap_err();
-                freespace.insert(free_idx, next_file); 
+                freespace.insert(free_idx, next_file);
                 compact_around(free_idx, &mut freespace);
             }
         }
-
     }
     let mut checksum = 0;
     for c in compacted_files {
         for idx in c.start..c.end {
             checksum += idx as u64 * c.id as u64;
         }
-
     }
-    return checksum
+    checksum
 }
 
-fn find_next_free(file_len: usize, freespace_vec: &Vec<File>) -> Option<usize>{
+fn find_next_free(file_len: usize, freespace_vec: &[File], max_start: usize) -> Option<usize> {
     for (idx, freespace) in freespace_vec.iter().enumerate() {
-        if freespace.end - freespace.start >= file_len {
-            return Some(idx)
+        if freespace.start > max_start {
+            return None;
         }
-    } 
-    return None
+        if freespace.end - freespace.start >= file_len {
+            return Some(idx);
+        }
+    }
+    None
 }
 
 fn compact_around(idx: usize, freespace_vec: &mut Vec<File>) {
-    if idx < freespace_vec.len() - 1 {
-        if freespace_vec[idx+1].start == freespace_vec[idx].end {
-            freespace_vec[idx].end = freespace_vec[idx+1].end;
-            freespace_vec.remove(idx+1);
-        }
+    if idx < freespace_vec.len() - 1 && freespace_vec[idx + 1].start == freespace_vec[idx].end {
+        freespace_vec[idx].end = freespace_vec[idx + 1].end;
+        freespace_vec.remove(idx + 1);
     }
-    if idx > 0 {
-        if freespace_vec[idx-1].end == freespace_vec[idx].start {
-            freespace_vec[idx-1].end = freespace_vec[idx].end;
-            freespace_vec.remove(idx);
-        }
+    if idx > 0 && freespace_vec[idx - 1].end == freespace_vec[idx].start {
+        freespace_vec[idx - 1].end = freespace_vec[idx].end;
+        freespace_vec.remove(idx);
     }
 }
 
