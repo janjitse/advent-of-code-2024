a702706f7a4c35afc55b9ed91f3ac9c9a07d00b4
Jan Jitse <janjitse@gmail.com>
2024-12-10 22:36:03 +0100

    Some simplifications and optimizations

--- src/day10.rs
+++ src/day10.rs
@@ -8,30 +8,28 @@ fn parse(input: &str) -> Vec<Vec<u32>> {
         .take_while(|line| !line.is_empty())
         .map(|line| line.chars().map(|x| x.to_digit(10).unwrap()).collect())
         .collect();
-    println!("Parsing: {:?}", time_start.elapsed().unwrap());
+    // println!("Parsing: {:?}", time_start.elapsed().unwrap());
     output1
 }
 
-use std::collections::{HashSet, VecDeque};
+use fxhash::FxHashSet;
+use std::collections::VecDeque;
 
 #[aoc(day10, part1)]
 fn part1(input: &str) -> i32 {
     let x = parse(input);
-    let mut trailheads = HashSet::new();
-    let mut trail_ends = HashSet::new();
+    let mut trailheads = vec![];
     for (row_idx, row) in x.iter().enumerate() {
         for (col_idx, &col) in row.iter().enumerate() {
             if col == 0 {
-                trailheads.insert((row_idx, col_idx));
-            } else if col == 9 {
-                trail_ends.insert((row_idx, col_idx));
+                trailheads.push((row_idx, col_idx));
             }
         }
     }
     let directions = [(0, 1), (1, 0), (usize::MAX, 0), (0, usize::MAX)];
     let mut output = 0;
     for start in trailheads {
-        let mut visited = HashSet::new();
+        let mut visited = FxHashSet::default();
         let mut todo = VecDeque::new();
         todo.push_back((start, 0));
         while let Some((next_node, height)) = todo.pop_front() {
@@ -43,12 +41,12 @@ fn part1(input: &str) -> i32 {
                         next_node.0.wrapping_add(dir.0),
                         next_node.1.wrapping_add(dir.1),
                     );
-                    if height == 8 && x[cand.0][cand.1] == 9 {
-                        visited.insert(cand);
-                        continue;
-                    }
                     if x[cand.0][cand.1] == height + 1 {
-                        todo.push_back((cand, height + 1));
+                        if height == 9 {
+                            visited.insert(cand);
+                        } else {
+                            todo.push_back((cand, height + 1));
+                        }
                     }
                 }
             }
@@ -56,30 +54,26 @@ fn part1(input: &str) -> i32 {
         // println!("trailhead {:?}, score {:?}", start, visited.len() );
         output += visited.len() as i32;
     }
-    return output;
+    output
 }
 
 #[aoc(day10, part2)]
 fn part2(input: &str) -> u64 {
     let x = parse(input);
-    let mut trailheads = HashSet::new();
-    let mut trail_ends = HashSet::new();
+    let mut trailheads = vec![];
     for (row_idx, row) in x.iter().enumerate() {
         for (col_idx, &col) in row.iter().enumerate() {
             if col == 0 {
-                trailheads.insert((row_idx, col_idx));
-            } else if col == 9 {
-                trail_ends.insert((row_idx, col_idx));
+                trailheads.push((row_idx, col_idx));
             }
         }
     }
     let directions = [(0, 1), (1, 0), (usize::MAX, 0), (0, usize::MAX)];
     let mut output = 0;
     for start in trailheads {
-        let mut trails = HashSet::new();
         let mut todo = VecDeque::new();
-        todo.push_back((start, 0, vec![start]));
-        while let Some((next_node, height, trail_so_far)) = todo.pop_front() {
+        todo.push_back((start, 0));
+        while let Some((next_node, height)) = todo.pop_front() {
             for dir in directions {
                 if next_node.0.wrapping_add(dir.0) < x.len()
                     && next_node.1.wrapping_add(dir.1) < x[0].len()
@@ -88,23 +82,18 @@ fn part2(input: &str) -> u64 {
                         next_node.0.wrapping_add(dir.0),
                         next_node.1.wrapping_add(dir.1),
                     );
-                    if height == 8 && x[cand.0][cand.1] == 9 {
-                        let mut new_trail = trail_so_far.clone();
-                        new_trail.push(cand);
-                        trails.insert(new_trail);
-                        continue;
-                    }
                     if x[cand.0][cand.1] == height + 1 {
-                        let mut new_trail = trail_so_far.clone();
-                        new_trail.push(cand);
-                        todo.push_back((cand, height + 1, new_trail));
+                        if height == 9 {
+                            output += 1;
+                        } else {
+                            todo.push_back((cand, height + 1));
+                        }
                     }
                 }
             }
         }
-        output += trails.len() as u64;
     }
-    return output;
+    output
 }
 
 #[cfg(test)]
