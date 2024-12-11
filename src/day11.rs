49cd1a3cbb3b493551fb4d640b84d9ef982892b7
Jan Jitse <janjitse@gmail.com>
2024-12-11 20:39:08 +0100

    Going wild with different implementations

--- src/day11.rs
+++ src/day11.rs
@@ -1,65 +1,181 @@
 use std::time::SystemTime;
 
-fn parse(input: &str) -> Vec<u128> {
+fn parse(input: &str) -> Vec<u64> {
     let time_start = SystemTime::now();
     let output1 = input
         .split_ascii_whitespace()
         .map(|x| x.parse().unwrap())
         .collect();
-    println!("Parsing: {:?}", time_start.elapsed().unwrap());
+    // println!("Parsing: {:?}", time_start.elapsed().unwrap());
     output1
-}
-
-#[aoc(day11, part1)]
-fn part1(input: &str) -> usize {
-    let mut output = parse(input);
-    for _ in 0..25 {
-        let mut next_elem = vec![];
-        // let prev_len = output.len();
-        for element in output {
-            if element == 0 {
-                next_elem.push(1)
-            } else if (element.checked_ilog10().unwrap_or(0) + 1) % 2 == 0 {
-                let length = element.checked_ilog10().unwrap_or(0) + 1;
-                next_elem.push(element / 10u128.pow(length / 2) as u128);
-                next_elem.push(element % 10u128.pow(length / 2) as u128);
-            } else {
-                next_elem.push(element * 2024);
-            }
-        }
-        // println!("Step {:?}, {:?}, {:?}, {:?}", idx, next_elem.len(), next_elem.len() / prev_len, next_elem.len() % prev_len );
-        // println!("Step {:?}, {:?}", idx, next_elem);
-        output = next_elem;
-    }
-
-    return output.len();
 }
 
 use fxhash::FxHashMap;
 
-#[aoc(day11, part2)]
-fn part2(input: &str) -> u128 {
+#[aoc(day11, part1)]
+fn part1(input: &str) -> u64 {
     let output = parse(input);
     let mut hash = FxHashMap::default();
     for d in output {
-        *hash.entry(d).or_insert(0 as u128) += 1;
+        *hash.entry(d).or_default() += 1;
+    }
+    for _ in 0..25 {
+        let mut next_hash = FxHashMap::default();
+        for (idx, amount) in hash {
+            if idx == 0 {
+                *next_hash.entry(1).or_default() += amount;
+            } else if (idx.checked_ilog10().unwrap_or(0) + 1) % 2 == 0 {
+                let length = idx.checked_ilog10().unwrap_or(0) + 1;
+                *next_hash.entry(idx / 10u64.pow(length / 2)).or_default() += amount;
+                *next_hash.entry(idx % 10u64.pow(length / 2)).or_default() += amount;
+            } else {
+                *next_hash.entry(idx * 2024).or_default() += amount;
+            }
+        }
+        hash = next_hash;
+    }
+    // println!("{:?}", hash);
+    let mut total_length = 0;
+    for d in hash.values() {
+        total_length += d;
+    }
+
+    total_length
+}
+
+#[aoc(day11, part1, rec)]
+fn part1_rec(input: &str) -> u64 {
+    let output = parse(input);
+    let mut total = 0;
+    let mut cache = FxHashMap::default();
+    for d in output {
+        total += recurse_mem(25, d, &mut cache)
+    }
+    total
+}
+
+fn recurse_mem(blinks_todo: u8, idx: u64, cache: &mut FxHashMap<(u8, u64), u64>) -> u64 {
+    if blinks_todo == 0 {
+        return 1;
+    }
+    if let Some(total) = cache.get(&(blinks_todo, idx)) {
+        return *total;
+    }
+    let total;
+    if idx == 0 {
+        total = recurse_mem(blinks_todo - 1, 1, cache);
+    } else if (idx.checked_ilog10().unwrap_or(0) + 1) % 2 == 0 {
+        let length = idx.checked_ilog10().unwrap_or(0) + 1;
+        total = recurse_mem(blinks_todo - 1, idx / 10u64.pow(length / 2), cache)
+            + recurse_mem(blinks_todo - 1, idx % 10u64.pow(length / 2), cache);
+    } else {
+        total = recurse_mem(blinks_todo - 1, idx * 2024, cache);
+    }
+    cache.insert((blinks_todo, idx), total);
+    total
+}
+
+type Counter = FxHashMap<u64, u64>;
+
+fn recurse_dict(blinks_todo: u8, idx: u64, cache: &mut FxHashMap<(u8, u64), Counter>) -> Counter {
+    if blinks_todo == 0 {
+        return Counter::from_iter(vec![(idx, 1u64)]);
+    }
+    if let Some(total) = cache.get(&(blinks_todo, idx)) {
+        return total.clone();
+    }
+    let mut total: Counter;
+    if idx == 0 {
+        total = recurse_dict(blinks_todo - 1, 1, cache);
+    } else if (idx.checked_ilog10().unwrap_or(0) + 1) % 2 == 0 {
+        let length = idx.checked_ilog10().unwrap_or(0) + 1;
+        total = recurse_dict(blinks_todo - 1, idx / 10u64.pow(length / 2), cache);
+        let total2 = recurse_dict(blinks_todo - 1, idx % 10u64.pow(length / 2), cache);
+        for (t_idx, t_amount) in total2 {
+            *total.entry(t_idx).or_default() += t_amount;
+        }
+    } else {
+        total = recurse_dict(blinks_todo - 1, idx * 2024, cache);
+    }
+    let s = total.clone();
+    cache.insert((blinks_todo, idx), s);
+    total
+}
+
+#[aoc(day11, part1, rec_dict)]
+fn part1_rec_dict(input: &str) -> u64 {
+    let output = parse(input);
+    let mut total = Counter::default();
+    let mut cache = FxHashMap::default();
+    for d in output {
+        for (idx, amount) in recurse_dict(25, d, &mut cache) {
+            *total.entry(idx).or_default() += amount;
+        }
+    }
+    let mut output = 0;
+    for d in total.values() {
+        output += *d;
+    }
+    output
+}
+
+#[aoc(day11, part2, rec_dict)]
+fn part2_rec_dict(input: &str) -> u64 {
+    let output = parse(input);
+    let mut total = Counter::default();
+    let mut cache = FxHashMap::default();
+    for d in output {
+        *total.entry(d).or_default() += 1;
+    }
+
+    for _ in 0..15 {
+        let mut new_hash = Counter::default();
+        for (d, orig_amount) in total {
+            for (idx, amount) in recurse_dict(5, d, &mut cache) {
+                *new_hash.entry(idx).or_default() += amount * orig_amount;
+            }
+        }
+        total = new_hash;
+    }
+
+    let mut output = 0;
+    for d in total.values() {
+        output += *d;
+    }
+    output
+}
+
+#[aoc(day11, part2, rec)]
+fn part2_rec(input: &str) -> u64 {
+    let output = parse(input);
+    let mut total = 0;
+    let mut cache = FxHashMap::default();
+    for d in output {
+        total += recurse_mem(75, d, &mut cache)
+    }
+    total
+}
+
+#[aoc(day11, part2)]
+fn part2(input: &str) -> u64 {
+    let output = parse(input);
+    let mut hash = FxHashMap::default();
+    for d in output {
+        *hash.entry(d).or_default() += 1;
     }
     for _ in 0..75 {
         let mut next_hash = FxHashMap::default();
         for (idx, amount) in hash {
             if idx == 0 {
-                *next_hash.entry(1).or_insert(0) += amount;
+                *next_hash.entry(1).or_default() += amount;
             } else if (idx.checked_ilog10().unwrap_or(0) + 1) % 2 == 0 {
                 let length = idx.checked_ilog10().unwrap_or(0) + 1;
-                *next_hash.entry(idx / 10u128.pow(length / 2)).or_insert(0) += amount;
-                *next_hash.entry(idx % 10u128.pow(length / 2)).or_insert(0) += amount;
+                *next_hash.entry(idx / 10u64.pow(length / 2)).or_default() += amount;
+                *next_hash.entry(idx % 10u64.pow(length / 2)).or_default() += amount;
             } else {
-                *next_hash.entry(idx * 2024).or_insert(0) += amount;
+                *next_hash.entry(idx * 2024).or_default() += amount;
             }
         }
-        // println!("Step {:?}, {:?}, {:?}, {:?}", idx, next_elem.len(), next_elem.len() / prev_len, next_elem.len() % prev_len );
-        // println!("Step {:?}, {:?}", idx, next_elem);
-        // println!("Step {:?}, {:?}", idx, next_hash);
         hash = next_hash;
     }
     let mut total_length = 0;
@@ -67,7 +183,7 @@ fn part2(input: &str) -> u128 {
         total_length += d;
     }
 
-    return total_length;
+    total_length
 }
 
 #[cfg(test)]
@@ -83,19 +199,11 @@ mod tests {
         assert_eq!(part1(&contents), 55312);
     }
 
-    // #[test]
-    // fn test_cycle() {
-    //     let s = Path::new(file!()).file_stem().unwrap().to_str().unwrap();
-    //     // let file_path = format!("input/2024/{}_small.txt", s);
-    //     // let contents = fs::read_to_string(file_path).expect("file not found");
-    //     let contents = "1".to_string();
-    //     assert_eq!(part1(&contents), 1);
-    // }
     #[test]
     fn test_2() {
         let s = Path::new(file!()).file_stem().unwrap().to_str().unwrap();
         let file_path = format!("input/2024/{}_small.txt", s);
         let contents = fs::read_to_string(file_path).expect("file not found");
-        assert_eq!(part2(&contents), 55312);
+        assert_eq!(part2(&contents), 65601038650482);
     }
 }
