8bd47d297e615e50eb56713f17d31177136c5f54
Jan Jitse <janjitse@gmail.com>
2024-12-12 08:08:14 +0100

    Cleanup and speedup

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,6 @@
-use std::{collections::{HashSet, VecDeque}, time::SystemTime};
+use std::{collections::VecDeque, time::SystemTime};
+
+use fxhash::FxHashSet;
 
 fn parse(input: &str) -> Vec<Vec<char>> {
     let time_start = SystemTime::now();
@@ -12,38 +14,23 @@ fn parse(input: &str) -> Vec<Vec<char>> {
     output1
 }
 
-
-use fxhash::FxHashSet;
-
 #[aoc(day12, part1)]
 fn part1(input: &str) -> u64 {
-    let x = parse(input);
-    let mut done: FxHashSet<(usize,usize)> = FxHashSet::default();
+    let mut x = parse(input);
+    let height = x.len();
+    let width = x[0].len();
     let mut regions = vec![];
-    loop {
-        let mut current_char = '_';
-        let mut current_region: FxHashSet<(usize,usize)> = FxHashSet::default();
-        for (row_idx, row) in x.iter().enumerate() {
-            for (col_idx, &c) in row.iter().enumerate() {
-                if done.contains(&(row_idx, col_idx)) {
-                    continue;
+    for row_idx in 0..height {
+        for col_idx in 0..width {
+            if x[row_idx][col_idx] != '.' {
+                let current_char = x[row_idx][col_idx];
+                let current_region = dfs(&x, current_char, (row_idx, col_idx));
+                for d in current_region.clone().iter() {
+                    x[d.0][d.1] = '.';
                 }
-                else if current_char == '_' {
-                    current_char = c;
-                    current_region = dfs(&x, current_char, (row_idx, col_idx));
-                    // done = done.union(&current_region.iter().map(|x| *x)).collect();
-                    for d in current_region.clone().iter() {
-                        done.insert(*d);
-                    }
-                }
-
+                regions.push(current_region);
             }
         }
-        if current_char == '_' {
-            break;
-        }
-        regions.push(current_region);
-
     }
     let mut output = 0;
     for region in regions {
@@ -51,7 +38,7 @@ fn part1(input: &str) -> u64 {
         let mut perimeter = 0;
         for c in region.iter() {
             area += 1;
-            for dir in [(0,1),(1,0),(0,usize::MAX),(usize::MAX,0)] {
+            for dir in [(0, 1), (1, 0), (0, usize::MAX), (usize::MAX, 0)] {
                 if !region.contains(&(c.0.wrapping_add(dir.0), c.1.wrapping_add(dir.1))) {
                     perimeter += 1;
                 }
@@ -64,132 +51,92 @@ fn part1(input: &str) -> u64 {
     output
 }
 
-
-
-fn dfs(x: &Vec<Vec<char>>, cur_char: char, start_loc: (usize,usize)) -> FxHashSet<(usize,usize)> {
+fn dfs(x: &[Vec<char>], cur_char: char, start_loc: (usize, usize)) -> FxHashSet<(usize, usize)> {
     let mut region = FxHashSet::default();
     let mut queue = VecDeque::new();
     queue.push_back(start_loc);
     region.insert(start_loc);
     while let Some(next_c) = queue.pop_front() {
-        for dir in [(0,1),(1,0),(usize::MAX,0),(0,usize::MAX)] {
+        for dir in [(0, 1), (1, 0), (usize::MAX, 0), (0, usize::MAX)] {
             let next_loc = (next_c.0.wrapping_add(dir.0), next_c.1.wrapping_add(dir.1));
-            if next_loc.0 < x.len() && next_loc.1 < x[0].len() && x[next_loc.0][next_loc.1] == cur_char {
-                if !region.contains(&next_loc) {
-                    queue.push_back(next_loc);
-                    region.insert(next_loc);
-                }
+            if next_loc.0 < x.len()
+                && next_loc.1 < x[0].len()
+                && x[next_loc.0][next_loc.1] == cur_char
+                && !region.contains(&next_loc)
+            {
+                queue.push_back(next_loc);
+                region.insert(next_loc);
             }
         }
     }
-    return region;
+    region
 }
 #[aoc(day12, part2)]
 fn part2(input: &str) -> i32 {
-    let x = parse(input);
-    let mut done: FxHashSet<(usize,usize)> = FxHashSet::default();
+    let mut x = parse(input);
+    let height = x.len();
+    let width = x[0].len();
     let mut regions = vec![];
-    loop {
-        let mut current_char = '_';
-        let mut current_region: FxHashSet<(usize,usize)> = FxHashSet::default();
-        for (row_idx, row) in x.iter().enumerate() {
-            for (col_idx, &c) in row.iter().enumerate() {
-                if done.contains(&(row_idx, col_idx)) {
-                    continue;
+    for row_idx in 0..height {
+        for col_idx in 0..width {
+            if x[row_idx][col_idx] != '.' {
+                let current_char = x[row_idx][col_idx];
+                let current_region = dfs(&x, current_char, (row_idx, col_idx));
+                for d in current_region.clone().iter() {
+                    x[d.0][d.1] = '.';
                 }
-                else if current_char == '_' {
-                    current_char = c;
-                    current_region = dfs(&x, current_char, (row_idx, col_idx));
-                    // done = done.union(&current_region.iter().map(|x| *x)).collect();
-                    for d in current_region.clone().iter() {
-                        done.insert(*d);
-                    }
-                }
-
+                regions.push(current_region);
             }
         }
-        if current_char == '_' {
-            break;
-        }
-        regions.push(current_region);
-
     }
     let mut output = 0;
     for region in regions {
         let mut area = 0;
         let mut corners = 0;
-        let mut inner_corners = 0;
-        // let mut inner_corners = 0;
         for c in region.iter() {
             area += 1;
-            // let mut edge = vec![0, 0, 0, 0];
-            // for (idx, dir) in [(0,1),(1,0),(0,usize::MAX),(usize::MAX,0)].into_iter().enumerate() {
-            //     if !region.contains(&(c.0.wrapping_add(dir.0), c.1.wrapping_add(dir.1))) {
-            //         // perimeter += 1;
-            //         edge[idx] = 1;
-            //     }
-            // }
-            // //This counts outer corners
-            // if edge[0] == 1 && edge[1] == 1 {
-            //     outer_corners += 2;
-            // }
-            // if edge[1] == 1 && edge[2] == 1 {
-            //     outer_corners += 2;
-            // }
-            // if edge[2] == 1 && edge[3] == 1 {
-            //     outer_corners += 2;
-            // }
-            // if edge[3] == 1 && edge[0] == 1 {
-            //     outer_corners += 2;
-            // }
-            let mut conv = vec![vec![0,0,0],vec![0,0,0], vec![0,0,0]];
-            // conv[1][1] = 1
-            for dir in [(0,1),(1,1),(1,0),(usize::MAX,0),(0,usize::MAX),(1,usize::MAX),(usize::MAX,usize::MAX),(usize::MAX,1)] {
+            let mut conv = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+            for dir in [
+                (0, 1),
+                (1, 1),
+                (1, 0),
+                (usize::MAX, 0),
+                (0, usize::MAX),
+                (1, usize::MAX),
+                (usize::MAX, usize::MAX),
+                (usize::MAX, 1),
+            ] {
                 if !region.contains(&(c.0.wrapping_add(dir.0), c.1.wrapping_add(dir.1))) {
-                    // perimeter += 1;
                     conv[1usize.wrapping_add(dir.0)][1usize.wrapping_add(dir.1)] = 1;
                 }
             }
+            // outer corners
+            for w in [(0, 1), (1, 2), (2, 1), (1, 0), (0, 1)].windows(2) {
+                if conv[w[0].0][w[0].1] == 1 && conv[w[1].0][w[1].1] == 1 {
+                    corners += 1;
+                }
+            }
 
-            //outer corners
-            if conv[0][1] == 1 && conv[1][2] == 1 {
-                corners += 2;
-                inner_corners +=2;
-            }
-            if conv[1][2] == 1 && conv[2][1] == 1 {
-                corners += 2;
-                inner_corners +=2;
-            }
-            if conv[2][1] == 1 && conv[1][0] == 1 {
-                corners += 2;
-                inner_corners +=2;
-            }
-            if conv[1][0] == 1 && conv[0][1] == 1 {
-                corners += 2;
-                inner_corners +=2;
-            }
             //inner corners
             if conv[0][1] == 0 && conv[1][2] == 0 && conv[0][2] == 1 {
-                corners += 2;
+                corners += 1;
             }
             if conv[1][2] == 0 && conv[2][1] == 0 && conv[2][2] == 1 {
-                corners += 2;
+                corners += 1;
             }
             if conv[2][1] == 0 && conv[1][0] == 0 && conv[2][0] == 1 {
-                corners += 2;
+                corners += 1;
             }
             if conv[1][0] == 0 && conv[0][1] == 0 && conv[0][0] == 1 {
-                corners += 2;
+                corners += 1;
             }
-            
         }
         // println!("{:?}, {:?}, {:?}", area, inner_corners, corners-inner_corners);
-        output += area * corners/2;
+        output += area * corners;
     }
 
     output
 }
-
 
 #[cfg(test)]
 mod tests {
