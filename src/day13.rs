aa3cc9a8e85dcd3d2764a173cf8c9f953980c3fb
Jan Jitse <janjitse@gmail.com>
2024-12-13 20:53:09 +0100

    Formatting and cleanup of the input parsing

--- src/day13.rs
+++ src/day13.rs
@@ -1,5 +1,5 @@
-use std::time::SystemTime;
 use regex::Regex;
+use std::time::SystemTime;
 
 #[derive(Debug)]
 struct Entry {
@@ -10,32 +10,25 @@ struct Entry {
 
 fn parse(input: &str) -> Vec<Entry> {
     let time_start = SystemTime::now();
-    let lines = input.split("\r\n\r\n");
-    let mut full_output = vec![];
-    let regex = Regex::new(r"X(?:=|\+)(\d+), Y(?:=|\+)(\d+)").unwrap();
-    for l in lines {
-        let mut button_a:(u64, u64) = (0,0);
-        let mut button_b:(u64, u64) = (0,0);
-        let mut prize:(u64, u64) = (0,0);
-        for (idx, p) in l.split("\r\n").enumerate() {
-            if idx == 0 {
-                let captures = regex.captures(p).unwrap();
-                button_a = (captures[1].parse::<u64>().unwrap(), captures[2].parse::<u64>().unwrap());
-            }
-            if idx == 1 {
-                let captures = regex.captures(p).unwrap();
-                button_b = (captures[1].parse::<u64>().unwrap(), captures[2].parse::<u64>().unwrap());
-
-            }
-            if idx == 2 {
-                let captures = regex.captures(p).unwrap();
-                prize = (captures[1].parse::<u64>().unwrap(), captures[2].parse::<u64>().unwrap());
-            }
-
-
-        }
+    let lines = input.lines().collect::<Vec<_>>();
+    let mut full_output = Vec::with_capacity(lines.len() / 4);
+    let regex = Regex::new(r"X(?:=|\+)(?<x>\d+), Y(?:=|\+)(?<y>\d+)").unwrap();
+    for l in lines.chunks(4) {
+        let buttons_prize: Vec<_> = l
+            .iter()
+            .filter(|x| !x.is_empty())
+            .map(|x| {
+                let cap = regex.captures(x).unwrap();
+                (
+                    cap["x"].parse::<u64>().unwrap(),
+                    cap["y"].parse::<u64>().unwrap(),
+                )
+            })
+            .collect();
         let total = Entry {
-            button_a, button_b, prize
+            button_a: buttons_prize[0],
+            button_b: buttons_prize[1],
+            prize: buttons_prize[2],
         };
         full_output.push(total);
     }
@@ -49,18 +42,24 @@ fn part1(input: &str) -> u64 {
     let x = parse(input);
     let mut total = 0;
     for entry in x {
-        let det = (entry.button_a.0 * entry.button_b.1) as i64 - (entry.button_a.1 * entry.button_b.0) as i64;
+        let det = (entry.button_a.0 * entry.button_b.1) as i64
+            - (entry.button_a.1 * entry.button_b.0) as i64;
         assert_ne!(det, 0);
-        let raw_a = entry.prize.0 as i64 *  entry.button_b.1 as i64 - entry.button_b.0 as i64 * entry.prize.1 as i64;
-        let raw_b = entry.prize.1 as i64 *  entry.button_a.0 as i64 - entry.button_a.1 as i64 * entry.prize.0 as i64;
-        if raw_a % det == 0 && raw_b % det == 0 {
-            if raw_a / det >= 0 && raw_b / det >= 0  && raw_a / det <= 100 && raw_b / det <= 100 {
-                total += (3 * raw_a / det + raw_b / det) as u64; 
-            }
-            
+        let raw_a = entry.prize.0 as i64 * entry.button_b.1 as i64
+            - entry.button_b.0 as i64 * entry.prize.1 as i64;
+        let raw_b = entry.prize.1 as i64 * entry.button_a.0 as i64
+            - entry.button_a.1 as i64 * entry.prize.0 as i64;
+        if raw_a % det == 0
+            && raw_b % det == 0
+            && raw_a / det >= 0
+            && raw_b / det >= 0
+            && raw_a / det <= 100
+            && raw_b / det <= 100
+        {
+            total += (3 * raw_a / det + raw_b / det) as u64;
         }
     }
-    return total
+    total
 }
 
 #[aoc(day13, part2)]
@@ -70,22 +69,19 @@ fn part2(input: &str) -> u64 {
     for mut entry in x {
         entry.prize.0 += 10000000000000;
         entry.prize.1 += 10000000000000;
-        let det = (entry.button_a.0 * entry.button_b.1) as i64 - (entry.button_a.1 * entry.button_b.0) as i64;
+        let det = (entry.button_a.0 * entry.button_b.1) as i64
+            - (entry.button_a.1 * entry.button_b.0) as i64;
         assert_ne!(det, 0);
-        let raw_a = entry.prize.0 as i64 *  entry.button_b.1 as i64 - entry.button_b.0 as i64 * entry.prize.1 as i64;
-        let raw_b = entry.prize.1 as i64 *  entry.button_a.0 as i64 - entry.button_a.1 as i64 * entry.prize.0 as i64;
-        if raw_a % det == 0 && raw_b % det == 0 {
-            if raw_a / det >= 0 && raw_b / det >= 0 { // && raw_a / det <= 100 && raw_b / det <= 100 {
-                total += (3 * raw_a / det + raw_b / det) as u64; 
-            }
-            
+        let raw_a = entry.prize.0 as i64 * entry.button_b.1 as i64
+            - entry.button_b.0 as i64 * entry.prize.1 as i64;
+        let raw_b = entry.prize.1 as i64 * entry.button_a.0 as i64
+            - entry.button_a.1 as i64 * entry.prize.0 as i64;
+        if raw_a % det == 0 && raw_b % det == 0 && raw_a / det >= 0 && raw_b / det >= 0 {
+            total += (3 * raw_a / det + raw_b / det) as u64;
         }
     }
-    return total
+    total
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -107,5 +103,4 @@ mod tests {
         let contents = fs::read_to_string(file_path).expect("file not found");
         assert_eq!(part2(&contents), 875318608908);
     }
-
 }
