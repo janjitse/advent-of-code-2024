497ca85630f0a4f084cb961d99badf0fe90999d1
Jan Jitse <janjitse@gmail.com>
2024-12-15 16:22:56 +0100

    Slightly more general (allows for arbitrary shape boxes) (and shorter) version

--- src/day15.rs
+++ src/day15.rs
@@ -216,6 +216,7 @@ fn part2(input: &str) -> u64 {
         .sum()
 }
 
+#[allow(dead_code)]
 fn print_map(
     map_size: (usize, usize),
     left_boxes: &FxHashSet<(usize, usize)>,
@@ -247,6 +248,119 @@ fn print_map(
     for l in new_map.iter() {
         println!("{:?}", l.iter().collect::<String>());
     }
+}
+
+#[aoc(day15, part2, ids)]
+fn part2_ids(input: &str) -> u64 {
+    let (map, movement) = parse(input);
+    let mut robot_location = (0, 0);
+    let mut obstacles = FxHashSet::default();
+    let mut boxes_loc = FxHashMap::default();
+    let mut loc_box = FxHashMap::default();
+    let mut box_id = 0;
+    for (idx_row, row) in map.iter().enumerate() {
+        for (idx_col, &col) in row.iter().enumerate() {
+            if col == '@' {
+                robot_location = (idx_row, 2 * idx_col);
+            } else if col == '#' {
+                obstacles.insert((idx_row, 2 * idx_col));
+                obstacles.insert((idx_row, 2 * idx_col + 1));
+            } else if col == 'O' {
+                for w in [0, 1] {
+                    loc_box
+                        .entry(box_id)
+                        .or_insert(vec![])
+                        .push((idx_row, 2 * idx_col + w));
+                    boxes_loc.insert((idx_row, 2 * idx_col + w), box_id);
+                }
+                box_id += 1;
+            }
+        }
+    }
+    let robot_movement = FxHashMap::from_iter([
+        ('v', (1, 0)),
+        ('>', (0, 1)),
+        ('<', (0, usize::MAX)),
+        ('^', (usize::MAX, 0)),
+    ]);
+    for m in movement {
+        // print_map(&map, &left_boxes, &right_boxes, &obstacles, &robot_location);
+        let robot_dir = robot_movement[&m];
+        // println!("Trying direction: {:?}", robot_dir);
+        let new_robot_loc = (
+            robot_location.0.wrapping_add(robot_dir.0),
+            robot_location.1.wrapping_add(robot_dir.1),
+        );
+        // println!("New trial loc: {:?}", new_robot_loc);
+        if obstacles.contains(&new_robot_loc) {
+            // println!("Obstacle found at {:?}", new_robot_loc);
+            continue;
+        }
+        if let Some(box_id) = boxes_loc.get(&new_robot_loc) {
+            let mut check = VecDeque::new();
+            let mut box_bloc = FxHashSet::default();
+            for &b in loc_box[box_id].iter() {
+                box_bloc.insert(b);
+                check.push_back(b);
+            }
+            while let Some(box_part) = check.pop_front() {
+                let next_box = (
+                    box_part.0.wrapping_add(robot_dir.0),
+                    box_part.1.wrapping_add(robot_dir.1),
+                );
+                if let Some(next_box_id) = boxes_loc.get(&next_box) {
+                    for &b in loc_box[next_box_id].iter() {
+                        let new = box_bloc.insert(b);
+                        if new {
+                            check.push_back(b);
+                        }
+                    }
+                }
+            }
+            let mut possible = true;
+            for b in box_bloc.iter() {
+                if obstacles
+                    .contains(&(b.0.wrapping_add(robot_dir.0), b.1.wrapping_add(robot_dir.1)))
+                {
+                    possible = false;
+                    break;
+                }
+            }
+            // println!("Moving {:?}", box_bloc);
+            if possible {
+                // Get all box ids, and remove the locations from boxes_loc
+                // Get location from loc_box, and update, and add to boxes_loc again
+                let box_ids = FxHashSet::from_iter(
+                    box_bloc.into_iter().map(|x| boxes_loc.remove(&x).unwrap()),
+                );
+                for b_i in box_ids {
+                    let locs = loc_box.get_mut(&b_i).unwrap();
+                    for loc_idx in 0..locs.len() {
+                        let new_loc = (
+                            locs[loc_idx].0.wrapping_add(robot_dir.0),
+                            locs[loc_idx].1.wrapping_add(robot_dir.1),
+                        );
+                        locs[loc_idx] = new_loc;
+                        boxes_loc.insert(new_loc, b_i);
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+        // println!("Location updated to: {:?}", new_robot_loc);
+        robot_location = new_robot_loc;
+        // print_map(&map, &left_boxes, &right_boxes, &obstacles, &robot_location);
+    }
+    // print_map((map.len(), map[0].len()), &left_boxes, &right_boxes, &obstacles, &robot_location, 2);
+
+    loc_box
+        .values()
+        .map(|x| {
+            x.iter().min_by_key(|y| y.0).unwrap().0 as u64 * 100
+                + x.iter().min_by_key(|y| y.1).unwrap().1 as u64
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -283,6 +397,14 @@ mod tests {
         let s = Path::new(file!()).file_stem().unwrap().to_str().unwrap();
         let file_path = format!("input/2024/{}_tiny2.txt", s);
         let contents = fs::read_to_string(file_path).expect("file not found");
-        assert_eq!(part2(&contents), 9021);
+        assert_eq!(part2(&contents), 618);
+    }
+
+    #[test]
+    fn test_2_tiny_ids() {
+        let s = Path::new(file!()).file_stem().unwrap().to_str().unwrap();
+        let file_path = format!("input/2024/{}_tiny2.txt", s);
+        let contents = fs::read_to_string(file_path).expect("file not found");
+        assert_eq!(part2_ids(&contents), 618);
     }
 }
