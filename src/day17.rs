6a91bd7a9e6f5892bb5ae78f7882d95544e6cbc6
Jan Jitse <janjitse@gmail.com>
2024-12-17 08:46:02 +0100

    Actual day 17

--- src/day17.rs
+++ src/day17.rs
@@ -6,40 +6,42 @@ fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
     let output1 = lines
         .by_ref()
         .take_while(|line| !line.is_empty())
-        .map(
-            |line| line.split(": ")
-            .skip(1)
-            .map(
-                |x| x.parse::<u64>().unwrap()
-            ).next().unwrap()
-        )
+        .map(|line| {
+            line.split(": ")
+                .skip(1)
+                .map(|x| x.parse::<u64>().unwrap())
+                .next()
+                .unwrap()
+        })
         .collect::<Vec<u64>>();
-    let output2 = lines.by_ref()
+    let output2 = lines
+        .by_ref()
         .take_while(|line| !line.is_empty())
-        .map(
-            |line| line.split(": ")
+        .map(|line| {
+            line.split(": ")
                 .skip(1)
                 .next()
                 .unwrap()
                 .split(",")
-                .map(
-                    |y| y.parse::<u64>().unwrap()
-                ).collect::<Vec<u64>>()
-            ).next().unwrap();
+                .map(|y| y.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>()
+        })
+        .next()
+        .unwrap();
     println!("Parsing: {:?}", time_start.elapsed().unwrap());
     return (output1, output2);
 }
 
 #[derive(Debug)]
 enum Opcode {
-    Adv (u64),
-    Bxl (u64),
-    Bst (u64),
-    Jnz (u64),
-    Bxc (u64),
-    Out (u64),
-    Bdv (u64),
-    Cdv (u64),
+    Adv(u64),
+    Bxl(u64),
+    Bst(u64),
+    Jnz(u64),
+    Bxc(u64),
+    Out(u64),
+    Bdv(u64),
+    Cdv(u64),
 }
 
 fn combo(v: u64, registers: &Vec<u64>) -> u64 {
@@ -55,8 +57,9 @@ fn combo(v: u64, registers: &Vec<u64>) -> u64 {
 #[aoc(day17, part1)]
 fn part1(input: &str) -> u64 {
     let (mut registers, instructions) = parse(input);
-    let opcodes: Vec<Opcode> = instructions.chunks(2).map(|x| 
-        match x[0] {
+    let opcodes: Vec<Opcode> = instructions
+        .chunks(2)
+        .map(|x| match x[0] {
             0 => Opcode::Adv(x[1]),
             1 => Opcode::Bxl(x[1]),
             2 => Opcode::Bst(x[1]),
@@ -65,30 +68,38 @@ fn part1(input: &str) -> u64 {
             5 => Opcode::Out(x[1]),
             6 => Opcode::Bdv(x[1]),
             7 => Opcode::Cdv(x[1]),
-            _ => panic!()
-        }
-    ).collect();
+            _ => panic!(),
+        })
+        .collect();
     println!("{:?}", opcodes);
     // return 0;
     let mut cur_pointer = 0;
     loop {
         match opcodes[cur_pointer] {
-            Opcode::Adv(v) => {registers[0] >>= combo(v, &registers);},
-            Opcode::Bxl(v) => {registers[1] = registers[1]^v;},
-            Opcode::Bst(v) => {registers[1] = combo(v, &registers)%8;},
-            Opcode::Jnz(v) => {if registers[0] != 0 {
-                cur_pointer = v as usize;
-                continue;
-            }},
+            Opcode::Adv(v) => {
+                registers[0] >>= combo(v, &registers);
+            }
+            Opcode::Bxl(v) => {
+                registers[1] = registers[1] ^ v;
+            }
+            Opcode::Bst(v) => {
+                registers[1] = combo(v, &registers) % 8;
+            }
+            Opcode::Jnz(v) => {
+                if registers[0] != 0 {
+                    cur_pointer = v as usize;
+                    continue;
+                }
+            }
             Opcode::Bxc(_v) => {
-                registers[1] = registers[1]^registers[2];
-            },
+                registers[1] = registers[1] ^ registers[2];
+            }
             Opcode::Out(v) => {
                 print!("{:?},", combo(v, &registers) % 8);
-            },
+            }
             Opcode::Bdv(v) => {
                 registers[1] = registers[0] >> combo(v, &registers);
-            },
+            }
             Opcode::Cdv(v) => {
                 registers[2] = registers[0] >> combo(v, &registers);
             }
@@ -100,15 +111,16 @@ fn part1(input: &str) -> u64 {
         }
     }
 
-    return 0
+    return 0;
 }
 
 #[aoc(day17, part2)]
 fn part2(input: &str) -> u64 {
     let (mut registers, instructions) = parse(input);
     let orig_instructions = instructions.clone();
-    let opcodes: Vec<Opcode> = instructions.chunks(2).map(|x| 
-        match x[0] {
+    let opcodes: Vec<Opcode> = instructions
+        .chunks(2)
+        .map(|x| match x[0] {
             0 => Opcode::Adv(x[1]),
             1 => Opcode::Bxl(x[1]),
             2 => Opcode::Bst(x[1]),
@@ -117,59 +129,58 @@ fn part2(input: &str) -> u64 {
             5 => Opcode::Out(x[1]),
             6 => Opcode::Bdv(x[1]),
             7 => Opcode::Cdv(x[1]),
-            _ => panic!()
-        }
-    ).collect();
+            _ => panic!(),
+        })
+        .collect();
     println!("{:?}", opcodes);
     // return 0;
-    
+
     // let old_registers = registers.clone();
     let output = recurse(orig_instructions.len() - 1, &orig_instructions, 1, &opcodes);
-    
 
-   
     // println!("{:?}", output.len());
-    return output.unwrap()
+    return output.unwrap();
 }
 
-
 fn recurse(depth: usize, target: &Vec<u64>, so_far: u64, opcodes: &Vec<Opcode>) -> Option<u64> {
-    println!("{:?}, {:?}", depth, so_far);
     if depth == 0 {
         if run(so_far, opcodes) == *target {
             return Some(so_far);
         } else {
-            return None
+            return None;
         }
     }
     let mut min_outcome = u64::MAX;
-    for trial in 8*so_far..8*so_far + 8 {
-        println!("{:?}", run(trial, opcodes));
-        println!("{:?}", &target[depth-1..]);
-        if run(trial, opcodes) == &target[depth-1..] {
-            if let Some(outcome) = recurse(depth-1, target, trial, opcodes ) {
+    for trial in 8 * so_far..8 * so_far + 8 {
+        if run(trial, opcodes) == &target[depth - 1..] {
+            if let Some(outcome) = recurse(depth - 1, target, trial, opcodes) {
                 min_outcome = min_outcome.min(outcome);
             }
         }
     }
     if min_outcome < u64::MAX {
-        return Some(min_outcome)
+        return Some(min_outcome);
     }
-    
 
-    return None
+    return None;
 }
 
-fn run(register_a: u64, opcodes: &Vec<Opcode> ) -> Vec<u64> {
+fn run(register_a: u64, opcodes: &Vec<Opcode>) -> Vec<u64> {
     let mut registers = vec![register_a, 0, 0];
     let mut cur_pointer = 0;
     let mut output = vec![];
     loop {
         // println!("{:?}", cur_pointer);
         match opcodes[cur_pointer] {
-            Opcode::Adv(v) => {registers[0] >>= combo(v, &registers);},
-            Opcode::Bxl(v) => {registers[1] = registers[1]^v;},
-            Opcode::Bst(v) => {registers[1] = combo(v, &registers)%8;},
+            Opcode::Adv(v) => {
+                registers[0] >>= combo(v, &registers);
+            }
+            Opcode::Bxl(v) => {
+                registers[1] = registers[1] ^ v;
+            }
+            Opcode::Bst(v) => {
+                registers[1] = combo(v, &registers) % 8;
+            }
             Opcode::Jnz(v) => {
                 // println!("A: {:?}", registers[0]);
                 if registers[0] != 0 {
@@ -177,17 +188,17 @@ fn run(register_a: u64, opcodes: &Vec<Opcode> ) -> Vec<u64> {
                     cur_pointer = v as usize;
                     continue;
                 }
-            },
+            }
             Opcode::Bxc(_v) => {
-                registers[1] = registers[1]^registers[2];
-            },
+                registers[1] = registers[1] ^ registers[2];
+            }
             Opcode::Out(v) => {
                 // println!("{:?}, {:?},", a, combo(v, &registers) % 8);
                 output.push(combo(v, &registers) % 8);
-            },
+            }
             Opcode::Bdv(v) => {
                 registers[1] = registers[0] >> combo(v, &registers);
-            },
+            }
             Opcode::Cdv(v) => {
                 registers[2] = registers[0] >> combo(v, &registers);
             }
@@ -199,7 +210,7 @@ fn run(register_a: u64, opcodes: &Vec<Opcode> ) -> Vec<u64> {
         }
     }
     // println!("{:?}, {:?}", a, output);
-    return output
+    return output;
 }
 
 #[cfg(test)]
