3ace1e0af28db2f7ef12cd2a75cce9df5d06b55f
Jan Jitse <janjitse@gmail.com>
2024-12-18 21:06:34 +0100

    Some pretty significant optimizations

--- src/day18.rs
+++ src/day18.rs
@@ -6,106 +6,131 @@ fn parse(input: &str) -> Vec<Vec<u64>> {
     let output1 = lines
         .by_ref()
         .take_while(|line| !line.is_empty())
-        .map(|line| {
-            line.split(",")
-                .map(|x| x.parse::<u64>().unwrap())
-                .collect()
-        })
+        .map(|line| line.split(",").map(|x| x.parse::<u64>().unwrap()).collect())
         .collect::<Vec<Vec<u64>>>();
     println!("Parsing: {:?}", time_start.elapsed().unwrap());
     output1
 }
 
 use fxhash::FxHashSet;
+use std::cmp::{Ordering, Reverse};
 use std::collections::BinaryHeap;
-use std::cmp::Reverse;
+
+#[derive(PartialEq, Eq)]
+struct Item {
+    dist: u64,
+    loc: (usize, usize),
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> Ordering {
+        Reverse(self.dist).cmp(&Reverse(other.dist))
+    }
+}
+
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(Reverse(self.dist).cmp(&Reverse(other.dist)))
+    }
+}
 
 #[aoc(day18, part1)]
 fn part1(input: &str) -> u64 {
     let x = parse(input);
-    let dir = [(0,1),(1,0),(0,usize::MAX), (usize::MAX,0)];
     let nr_bytes = match x.len() {
         25 => 12,
         3450 => 1024,
-        _ => panic!()
+        250999 => 1024,
+        251001 => 1024,
+        6254999 => 1024,
+        6255000 => 1024,
+        _ => panic!(),
     };
-    let mut corrupted = FxHashSet::default();
-    for idx in 0..nr_bytes {
-        corrupted.insert((x[idx][0] as usize, x[idx][1] as usize));
-    }
+    let visited = FxHashSet::from_iter(
+        x.iter()
+            .take(nr_bytes)
+            .map(|item| (item[0] as usize, item[1] as usize)),
+    );
+
     let end = match x.len() {
-        25 => (6,6),
-        3450 => (70,70),
-        _ => panic!()
+        25 => (6, 6),
+        3450 => (70, 70),
+        250999 => (500, 500),
+        251001 => (500, 500),
+        6254999 => (2500, 2500),
+        6255000 => (2500, 2500),
+        _ => panic!(),
     };
-    let mut visited = FxHashSet::default();
+    dijkstra((0, 0), end, visited).unwrap()
+}
+
+fn dijkstra(
+    start: (usize, usize),
+    end: (usize, usize),
+    mut visited: FxHashSet<(usize, usize)>,
+) -> Option<u64> {
     let mut p_q = BinaryHeap::new();
-    p_q.push((Reverse(0), (0 as usize,0 as usize)));
-    while let Some((dist, next_pos)) = p_q.pop() {
+    let dir = [(0, 1), (1, 0), (0, usize::MAX), (usize::MAX, 0)];
+    p_q.push(Item {
+        dist: 0,
+        loc: start,
+    });
+    while let Some(Item {
+        dist,
+        loc: next_pos,
+    }) = p_q.pop()
+    {
         if visited.contains(&next_pos) {
             continue;
         }
-        if (next_pos) == end {
-            return dist.0;
+        if next_pos == end {
+            return Some(dist);
         }
         visited.insert(next_pos);
         for d in &dir {
             let trial = (next_pos.0.wrapping_add(d.0), next_pos.1.wrapping_add(d.1));
-            if trial.0 <= end.0 && trial.1 <= end.1 {
-                if !corrupted.contains(&trial) {
-                    p_q.push((Reverse(dist.0 + 1), trial ));
-                }
+            if trial.0 <= end.0 && trial.1 <= end.1 && !visited.contains(&trial) {
+                p_q.push(Item {
+                    dist: dist + 1,
+                    loc: trial,
+                });
             }
         }
-    } 
-
-    return 0
+    }
+    None
 }
 
 #[aoc(day18, part2)]
 fn part2(input: &str) -> String {
     let x = parse(input);
-    let dir = [(0,1),(1,0),(0,usize::MAX), (usize::MAX,0)];
     let end = match x.len() {
-        25 => (6,6),
-        3450 => (70,70),
-        _ => panic!()
+        25 => (6, 6),
+        3450 => (70, 70),
+        250999 => (500, 500),
+        251001 => (500, 500),
+        6254999 => (2500, 2500),
+        6255000 => (2500, 2500),
+        6255001 => (2500, 2500),
+        _ => panic!(),
     };
-    for nr_bytes in 1..x.len() {
-        let mut corrupted = FxHashSet::default();
-        for idx in 0..nr_bytes {
-            corrupted.insert((x[idx][0] as usize, x[idx][1] as usize));
-        }
-        let mut visited = FxHashSet::default();
-        let mut p_q = BinaryHeap::new();
-        p_q.push((Reverse(0), (0 as usize,0 as usize)));
-        let mut exit_found = false;
-        while let Some((dist, next_pos)) = p_q.pop() {
-            if visited.contains(&next_pos) {
-                continue;
-            }
-            if (next_pos) == end {
-                exit_found = true;
-                break;
-            }
-            visited.insert(next_pos);
-            for d in &dir {
-                let trial = (next_pos.0.wrapping_add(d.0), next_pos.1.wrapping_add(d.1));
-                if trial.0 <= end.0 && trial.1 <= end.1 {
-                    if !corrupted.contains(&trial) {
-                        p_q.push((Reverse(dist.0 + 1), trial ));
-                    }
-                }
-            }
-        }
-        if !exit_found {
-            return format!("({},{})", x[nr_bytes-1][0],x[nr_bytes-1][1])
+    let mut low = 0;
+    let mut high = x.len() - 1;
+    while high > low {
+        let mid = (high + low + 1) / 2;
+        let visited = FxHashSet::from_iter(
+            x.iter()
+                .take(mid)
+                .map(|item| (item[0] as usize, item[1] as usize)),
+        );
+
+        if dijkstra((0, 0), end, visited).is_some() {
+            low = mid;
+        } else {
+            high = mid - 1;
         }
     }
-
-    return "(0,0)".to_string()
+    format!("{},{}", x[high][0], x[high][1])
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -125,6 +150,6 @@ mod tests {
         let s = Path::new(file!()).file_stem().unwrap().to_str().unwrap();
         let file_path = format!("input/2024/{}_small.txt", s);
         let contents = fs::read_to_string(file_path).expect("file not found");
-        assert_eq!(part2(&contents), "(6,1)".to_string());
+        assert_eq!(part2(&contents), "6,1".to_string());
     }
 }
