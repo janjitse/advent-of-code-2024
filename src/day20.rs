45ceca674c0ce5f37942c7c927dc829f7317b6ec
Jan Jitse <janjitse@gmail.com>
2024-12-20 08:37:38 +0100

    Major speed improvements + some refactoring

--- src/day20.rs
+++ src/day20.rs
@@ -1,53 +1,18 @@
 use std::time::SystemTime;
 
-fn parse(input: &str) -> Vec<Vec<char>> {
+fn parse(input: &str) -> (FxHashSet<(usize, usize)>, (usize, usize), (usize, usize)) {
     let time_start = SystemTime::now();
     let mut lines = input.lines();
-    let output1 = lines
+    let output1: Vec<Vec<char>> = lines
         .by_ref()
         .take_while(|line| !line.is_empty())
         .map(|line| line.chars().collect())
         .collect();
-    println!("Parsing: {:?}", time_start.elapsed().unwrap());
-    output1
-}
 
-use fxhash::{FxHashSet, FxHashMap};
-use std::cmp::{Ordering, Reverse};
-use std::collections::BinaryHeap;
-
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-struct Position {
-    loc: (usize, usize),
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct PriorityElement {
-    dist: u64,
-    pos: Position,
-    nr_cheats: u64,
-}
-
-impl Ord for PriorityElement {
-    fn cmp(&self, other: &Self) -> Ordering {
-        Reverse(self.dist).cmp(&Reverse(other.dist))
-    }
-}
-
-impl PartialOrd for PriorityElement {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(Reverse(self.dist).cmp(&Reverse(other.dist)))
-    }
-}
-
-#[aoc(day20, part1)]
-fn part1(input: &str) -> u64 {
-    let x = parse(input);
     let mut spaces = FxHashSet::default();
     let mut start = (0, 0);
     let mut end = (0, 0);
-    let mut obstacles = FxHashSet::default();
-    for (row_idx, row) in x.iter().enumerate() {
+    for (row_idx, row) in output1.iter().enumerate() {
         for (col_idx, &col) in row.iter().enumerate() {
             if col == '.' || col == 'S' || col == 'E' {
                 spaces.insert((row_idx, col_idx));
@@ -58,205 +23,111 @@ fn part1(input: &str) -> u64 {
             if col == 'S' {
                 start = (row_idx, col_idx);
             }
-            if col == '#' {
-                obstacles.insert((row_idx, col_idx));
-            }
         }
     }
+    println!("Parsing: {:?}", time_start.elapsed().unwrap());
+    (spaces, start, end)
+}
 
-    let clockwise = FxHashMap::from_iter([
-        ((0, 1), (1, 0)),
-        ((1, 0), (0, usize::MAX)),
-        ((0, usize::MAX), (usize::MAX, 0)),
-        ((usize::MAX, 0), (0, 1)),
-    ]);
+use fxhash::{FxHashMap, FxHashSet};
+
+#[aoc(day20, part1)]
+fn part1(input: &str) -> u64 {
+    let (spaces, start, end) = parse(input);
+
+    let directions = vec![(0, 1), (1, 0), (0, usize::MAX), (usize::MAX, 0)];
+    let mut distance_ord = FxHashMap::default();
+    let mut cur_loc = start;
+    distance_ord.insert(start, 0);
+    let mut cur_dist = 0;
+    while cur_loc != end {
+        for dir in directions.iter() {
+            let new_loc = (cur_loc.0.wrapping_add(dir.0), cur_loc.1.wrapping_add(dir.1));
+            if spaces.contains(&new_loc) && !distance_ord.contains_key(&new_loc) {
+                cur_loc = new_loc;
+                break;
+            }
+        }
+        cur_dist += 1;
+        distance_ord.insert(cur_loc, cur_dist);
+    }
+
     let mut cheating_paths = 0;
-    let len_to_visit = spaces.len() - 100;
-    for (cheat_idx, cheat) in obstacles.iter().enumerate() {
-        // println!("{:?}", cheat_idx);
-        let starting_pos = Position {
-            loc: start,
-        };
-        spaces.insert(*cheat);
-        let mut p_q = BinaryHeap::new();
-        let mut visited = FxHashSet::default();
-        p_q.push(PriorityElement {
-            dist: 0,
-            pos: starting_pos,
-            nr_cheats: 0,
-        });
-        let mut cheating_length = u64::MAX;
-        while let Some(PriorityElement {
-            dist: d,
-            pos,
-            nr_cheats: 0,
-        }) = p_q.pop()
-        {
-            if d as usize > len_to_visit {
-                break;
-            }
-            if pos.loc == end {
-                cheating_length  = d + 1;
-                break;
-            }
-            if visited.contains(&pos) {
-                continue;
-            }
-            visited.insert(pos.clone());
-            let mut cur_dir = (0,1);
-            for _ in 0..4 {
-                let next_pos = (
-                    pos.loc.0.wrapping_add(cur_dir.0),
-                    pos.loc.1.wrapping_add(cur_dir.1),
+    let full_distance = *distance_ord.get(&end).unwrap();
+    for cheat_start_loc in spaces.iter() {
+        let cheat_length_start = *distance_ord.get(cheat_start_loc).unwrap_or(&i64::MAX);
+        for y_delta in -2..=2 as isize {
+            for x_delta in -2..=2 as isize {
+                if x_delta.abs() + y_delta.abs() > 2 {
+                    continue;
+                }
+                let cheat_end_loc = (
+                    cheat_start_loc.0.wrapping_add(y_delta as usize),
+                    cheat_start_loc.1.wrapping_add(x_delta as usize),
                 );
-                if spaces.contains(&next_pos) {
-                    let xy = Position {
-                        loc: next_pos,
-                    };
-                    if !visited.contains(&xy) {
-                        p_q.push(PriorityElement {
-                            dist: d + 1,
-                            pos: xy,
-                            nr_cheats: 0,
-                        });
+                if distance_ord.contains_key(&cheat_end_loc) {
+                    let cheat_end_length = *distance_ord.get(&cheat_end_loc).unwrap();
+                    let cheat_length = x_delta.abs() + y_delta.abs();
+                    if cheat_length_start + (full_distance - cheat_end_length) + cheat_length as i64
+                        <= full_distance as i64 - 100
+                    {
+                        cheating_paths += 1;
                     }
                 }
-                cur_dir = clockwise[&cur_dir];
             }
         }
-        if cheating_length <= len_to_visit as u64 {
-            cheating_paths += 1;
-        }
-        spaces.remove(cheat);
     }
-
-    println!("{:?}", spaces.len());
-    println!("{:?}", len_to_visit);
     cheating_paths
 }
 
 #[aoc(day20, part2)]
 fn part2(input: &str) -> u64 {
-    let x = parse(input);
-    let mut spaces = FxHashSet::default();
-    let mut start = (0, 0);
-    let mut end = (0, 0);
-    let mut obstacles = FxHashSet::default();
-    for (row_idx, row) in x.iter().enumerate() {
-        for (col_idx, &col) in row.iter().enumerate() {
-            if col == '.' || col == 'S' || col == 'E' {
-                spaces.insert((row_idx, col_idx));
-            }
-            if col == 'E' {
-                end = (row_idx, col_idx);
-            }
-            if col == 'S' {
-                start = (row_idx, col_idx);
-            }
-            if col == '#' {
-                obstacles.insert((row_idx, col_idx));
+    let (spaces, start, end) = parse(input);
+
+    let directions = vec![(0, 1), (1, 0), (0, usize::MAX), (usize::MAX, 0)];
+    let mut distance_ord = FxHashMap::default();
+    let mut cur_loc = start;
+    distance_ord.insert(start, 0);
+    let mut cur_dist = 0;
+    while cur_loc != end {
+        for dir in directions.iter() {
+            let new_loc = (cur_loc.0.wrapping_add(dir.0), cur_loc.1.wrapping_add(dir.1));
+            if spaces.contains(&new_loc) && !distance_ord.contains_key(&new_loc) {
+                cur_loc = new_loc;
+                break;
             }
         }
+        cur_dist += 1;
+        distance_ord.insert(cur_loc, cur_dist);
     }
 
-    let directions = vec![(0,1),(1,0),(0,usize::MAX),(usize::MAX,0)];
-    let mut p_q = BinaryHeap::new();
-    let mut distance_ord = FxHashMap::default();
-    let starting_pos = Position {
-        loc: start,
-    };
-    p_q.push(PriorityElement {
-        dist: 0,
-        pos: starting_pos,
-        nr_cheats: 0,
-    });
-    let mut cheating_length = u64::MAX;
-    while let Some(PriorityElement {
-        dist: d,
-        pos,
-        nr_cheats: 0,
-    }) = p_q.pop()
-    {
-        if pos.loc == end {
-            distance_ord.insert(pos.loc.clone(), d as i64);
-            break;
-        }
-        if distance_ord.contains_key(&pos.loc) {
-            continue;
-        }
-        distance_ord.insert(pos.loc.clone(), d as i64);
-        let mut cur_dir = (0,1);
-        for cur_dir in directions.iter() {
-            let next_pos = (
-                pos.loc.0.wrapping_add(cur_dir.0),
-                pos.loc.1.wrapping_add(cur_dir.1),
-            );
-            if spaces.contains(&next_pos) {
-                let xy = Position {
-                    loc: next_pos,
-                };
-                if !distance_ord.contains_key(&xy.loc) {
-                    p_q.push(PriorityElement {
-                        dist: d + 1,
-                        pos: xy,
-                        nr_cheats: 0,
-                    });
+    let mut cheating_paths = 0;
+    let full_distance = *distance_ord.get(&end).unwrap();
+    for cheat_start_loc in spaces.iter() {
+        let cheat_length_start = *distance_ord.get(cheat_start_loc).unwrap_or(&i64::MAX);
+        for y_delta in -20..=20 as isize {
+            for x_delta in -20..=20 as isize {
+                if x_delta.abs() + y_delta.abs() > 20 {
+                    continue;
+                }
+                let cheat_end_loc = (
+                    cheat_start_loc.0.wrapping_add(y_delta as usize),
+                    cheat_start_loc.1.wrapping_add(x_delta as usize),
+                );
+                if distance_ord.contains_key(&cheat_end_loc) {
+                    let cheat_end_length = *distance_ord.get(&cheat_end_loc).unwrap();
+                    let cheat_length = x_delta.abs() + y_delta.abs();
+                    if cheat_length_start + (full_distance - cheat_end_length) + cheat_length as i64
+                        <= full_distance as i64 - 100
+                    {
+                        cheating_paths += 1;
+                    }
                 }
             }
         }
-    }    
-
-    let mut cheating_paths = 0;
-    // println!("{:?}", distance_ord);
-    // let len_to_visit = spaces.len() - 3;
-    let full_distance = *distance_ord.get(&end).unwrap();
-    let mut cheat_start_end = FxHashSet::default();
-    for (cheat_idx, cheat_start) in spaces.iter().enumerate() {
-        // println!("{:?}", cheat_idx);
-        
-        let mut cheat_start_loc = (0,0);
-        // for dir in directions.iter() {
-            // let start_loc = (cheat_start.0.wrapping_add(dir.0), cheat_start.1.wrapping_add(dir.1));
-            let start_loc = (cheat_start.0, cheat_start.1);
-            let cheat_length_start = *distance_ord.get(&start_loc).unwrap_or(&i64::MAX);
-            cheat_start_loc = start_loc;
-            
-        
-            if cheat_length_start == i64::MAX {
-                continue;
-            }
-            for &cheat_end in spaces.iter() {
-                // for dir in directions.iter() {
-                    let cheat_end_loc = (cheat_end.0, cheat_end.1);
-                    
-                    let cheat_end_length = *distance_ord.get(&cheat_end_loc).unwrap_or(&i64::MAX);
-                    if cheat_end_length == i64::MAX {
-                        continue;
-                    }
-                    if cheat_start_end.contains(&(cheat_start_loc, cheat_end_loc)) {
-                        continue;
-                    }
-                    let cheat_length = cheat_start.0.abs_diff(cheat_end_loc.0) as i64 +  cheat_start.1.abs_diff(cheat_end_loc.1) as i64;
-                    if cheat_length <= 20 {
-                        // println!("{:?}, {:?}, {:?}", cheat_start, cheat_end, cheat_length);
-                        if cheat_length_start + (full_distance - cheat_end_length) + cheat_length <= full_distance as i64 - 100 {
-                            // println!("start at {:?}, {:?}, end at {:?}, {:?}, distance start: {:?}, distance_end: {:?}, cheat_length {:?}",  cheat_start_loc, cheat_start, cheat_end, cheat_end_loc, cheat_length_start,  cheat_end_length, cheat_length);
-                            cheating_paths += 1;
-                            cheat_start_end.insert((cheat_start_loc, cheat_end_loc));
-                        }
-                    }
-                
-            // }
-        }
-
-        
     }
-
-    println!("{:?}", spaces.len());
-    // println!("{:?}", len_to_visit);
     cheating_paths
 }
-
 
 #[cfg(test)]
 mod tests {
