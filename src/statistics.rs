82def355f316dcd52632b014bc2c199f95bf75f7
Jan Jitse <janjitse@gmail.com>
2024-12-20 12:03:44 +0100

    Remove unneeded hashmap

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,15 +1,14 @@
-use rustc_hash::FxHashMap;
-
 pub fn entropy(array: &Vec<u8>) -> f64 {
-    let mut bincount: FxHashMap<u8, u32> = FxHashMap::default();
+    let mut bincount = vec![0; 256];
     for a in array {
-        *bincount.entry(*a).or_default() += 1;
+        bincount[*a as usize] += 1;
     }
     let total = array.len() as u32; // add 256 for smoothing?
     bincount
-        .values()
+        .into_iter()
+        .filter(|v| *v!=0)
         .map(|v| {
-            let p = (*v) as f64 / total as f64;
+            let p = v as f64 / total as f64;
             -p * p.log2()
         })
         .sum()
@@ -22,6 +21,7 @@ mod tests {
     #[test]
     fn test_1() {
         let array = (0..255).collect();
+        println!("{:?}", entropy(&array));
         assert!((entropy(&array) - 8.0).abs() < 0.01)
     }
 }
