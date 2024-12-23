6a7fbb3d9f5f0e5878925a9165cd559f2ada7395
Jan Jitse <janjitse@gmail.com>
2024-12-23 16:30:23 +0100

    Major speedup and refactor

--- src/day22.rs
+++ src/day22.rs
@@ -12,82 +12,54 @@ fn parse(input: &str) -> Vec<u64> {
     output1
 }
 
-fn mix(result: u64, x: u64) -> u64 {
-    result ^ x
+fn next_secret(mut secret: u32) -> u32 {
+    secret ^= secret << 6;
+    secret %= 16777216;
+    secret ^= secret >> 5;
+    secret %= 16777216;
+    secret ^= secret << 11;
+    secret % 16777216
 }
 
-fn prune(result: u64) -> u64 {
-    result % 16777216
-}
-
-fn next_secret(mut secret: u64) -> u64 {
-    secret = mix(secret << 6, secret);
-    secret = prune(secret);
-
-    secret = mix(secret >> 5, secret);
-    secret = prune(secret);
-    secret = mix(secret << 11, secret);
-    prune(secret)
-}
 #[aoc(day22, part1)]
 fn part1(input: &str) -> u64 {
     let x = parse(input);
-    println!("{:?}", x);
     let mut output = 0;
     for secret in x {
-        let mut real_secret = secret;
+        let mut real_secret = secret as u32;
         for _ in 0..2000 {
             real_secret = next_secret(real_secret);
         }
-        println!("{:?}", real_secret);
-        output += real_secret;
+        output += real_secret as u64;
     }
 
-    output
-}
-
-use rustc_hash::FxHashMap;
-
-fn first_occurences(vector: Vec<i32>) -> FxHashMap<(i32, i32, i32, i32), i32> {
-    let mut output = FxHashMap::default();
-    for w in vector.windows(5) {
-        let differences = (w[1] - w[0], w[2] - w[1], w[3] - w[2], w[4] - w[3]);
-        if !output.contains_key(&differences) {
-            output.insert(differences, w[4]);
-        }
-    }
     output
 }
 
 #[aoc(day22, part2)]
 fn part2(input: &str) -> u64 {
     let x = parse(input);
-    println!("{:?}", x);
-    let mut all_first_differences = FxHashMap::default();
-    for secret in x {
-        let mut vec_digits = Vec::with_capacity(2001);
-        vec_digits.push((secret % 10) as i32);
-        let mut real_secret = secret;
-        for _ in 0..2000 {
+    let size = 19 * 19 * 19 * 19;
+    let modulus = 19 * 19 * 19;
+    let mut all_first_differences = vec![0; size];
+    let mut last_seen = vec![-1; size];
+
+    for (sec_idx, secret) in x.into_iter().enumerate() {
+        let mut last_secret = (secret % 10) as usize;
+        let mut real_secret = secret as u32;
+        let mut key = 0;
+        for idx in 0..2000 {
             real_secret = next_secret(real_secret);
-            vec_digits.push((real_secret % 10) as i32);
-        }
-        let secret_differences = first_occurences(vec_digits);
-        // println!("{:?}", secret_differences.get(&(-2,1,-1,3)));
-        for (k, v) in secret_differences {
-            all_first_differences.entry(k).or_insert(vec![]).push(v);
-        }
-    }
-    let mut max_output = 0;
-    // println!("{:?}", all_first_differences.get(&(-2,1,-1,3)));
-    for (k, v) in all_first_differences {
-        let output_sum: i32 = v.iter().sum();
-        if output_sum as u64 > max_output {
-            // println!("{:?}", k);
-            max_output = output_sum as u64;
+            let difference = (real_secret % 10) as usize + 9 - last_secret;
+            key = (19 * (key % (modulus))) + difference;
+            if idx > 3 && last_seen[key] < sec_idx as i32 {
+                last_seen[key] = sec_idx as i32;
+                all_first_differences[key] += (real_secret % 10) as u64;
+            }
+            last_secret = (real_secret % 10) as usize;
         }
     }
-    return max_output;
+    all_first_differences.into_iter().max().unwrap()
 }
 
 #[cfg(test)]
